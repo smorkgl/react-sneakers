3479f6a517f35fae8d4fd5aba50b17150cb87e3c
smorkgl <catosrak@gmail.com>
2023-09-25 22:21:35 +0300

    init

---
+++ react-snakers/src/App.js
@@ -0,0 +1,31 @@
+
+
+function App() {
+  return (
+    <div className="wrapper">
+    <header>
+      <div className="headerLeft">
+      <img src ="img/logo.svg" />
+      <div className="headerInfo">
+      <h3>FRANKY STORE</h3>
+      <p>Магазин лучшей электроники</p>
+      </div>
+      </div>
+      <ul className="headerRight">
+      <li>
+        <svg /><span>1205 руб.</span>
+      </li>
+      <li>
+        <svg /> 
+      </li>
+      </ul>
+    </header>
+    <div className= "content">
+      <h1>Все кроссовки</h1>
+
+    </div>
+    </div>
+  );
+}
+
+export default App
